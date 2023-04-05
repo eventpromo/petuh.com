35a4c2c24273351008e3c94e4342d5b8e01e2c35
Sergey Guk <sergey.guk@dataart.com>
2023-04-05 16:01:06 +0200

    first

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,10 @@
-import Link from 'next/link';
-import { getPosts } from '../utils/mdx-utils';
-
 import Footer from '../components/Footer';
 import Header from '../components/Header';
 import Layout, { GradientBackground } from '../components/Layout';
-import ArrowIcon from '../components/ArrowIcon';
 import { getGlobalData } from '../utils/global-data';
 import SEO from '../components/SEO';
 
-export default function Index({ posts, globalData }) {
+export default function Index({ globalData }) {
   return (
     <Layout>
       <SEO title={globalData.name} description={globalData.blogTitle} />
@@ -18,32 +14,13 @@ export default function Index({ posts, globalData }) {
           {globalData.blogTitle}
         </h1>
         <ul className="w-full">
-          {posts.map((post) => (
-            <li
-              key={post.filePath}
-              className="md:first:rounded-t-lg md:last:rounded-b-lg backdrop-blur-lg bg-white dark:bg-black dark:bg-opacity-30 bg-opacity-10 hover:bg-opacity-20 dark:hover:bg-opacity-50 transition border border-gray-800 dark:border-white border-opacity-10 dark:border-opacity-10 border-b-0 last:border-b hover:border-b hovered-sibling:border-t-0"
-            >
-              <Link
-                as={`/posts/${post.filePath.replace(/\.mdx?$/, '')}`}
-                href={`/posts/[slug]`}
-              >
-                <a className="py-6 lg:py-10 px-6 lg:px-16 block focus:outline-none focus:ring-4">
-                  {post.data.date && (
-                    <p className="uppercase mb-3 font-bold opacity-60">
-                      {post.data.date}
-                    </p>
-                  )}
-                  <h2 className="text-2xl md:text-3xl">{post.data.title}</h2>
-                  {post.data.description && (
-                    <p className="mt-3 text-lg opacity-60">
-                      {post.data.description}
-                    </p>
-                  )}
-                  <ArrowIcon className="mt-4" />
-                </a>
-              </Link>
-            </li>
-          ))}
+          <br></br>
+          Одной темной ночью на прошедших выходных зародилась новая религия - петушарианство.
+Как известно, главный петушара - Серега, он же и мессия в нашей конфессии.
+          <br></br>
+Обряд посвящения петушары - получить пощёчину арчи от другого петушары (желательно при этом быть в говно). В эту религию нельзя войти если существуют какие-то обидки и недомолвки между петушарами.
+          <br></br>
+Пощёчина Арчи - это главный священный обряд петушарианства, момент высочайшего просветления и абсолютного прощения. Каждый может даровать другому абсолютное прощение, каждый в этой религии - Иисус, каждый этой в религии - петушара.
         </ul>
       </main>
       <Footer copyrightText={globalData.footerText} />
--- utils/global-data.js
+++ utils/global-data.js
@@ -1,13 +1,13 @@
 export const getGlobalData = () => {
   const name = process.env.BLOG_NAME
     ? decodeURI(process.env.BLOG_NAME)
-    : 'Jay Doe';
+    : 'Петушаринаство';
   const blogTitle = process.env.BLOG_TITLE
     ? decodeURI(process.env.BLOG_TITLE)
-    : 'Next.js Blog Theme';
+    : 'Новейшая религия';
   const footerText = process.env.BLOG_FOOTER_TEXT
     ? decodeURI(process.env.BLOG_FOOTER_TEXT)
-    : 'All rights reserved.';
+    : 'Во имени петуха, петушары и петушиного духа. АЛО!';
 
   return {
     name,
